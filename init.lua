f91aee776cc2e47ab4551f05ae4aacb6aa41c961
Giuseppe Salvi <giuseppe.salvi98@gmail.com>
2024-12-27 16:39:49 +0100

    fix: catppuccin config

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("config.keymaps")
+require("config.options")
+require("config.autocmds")
 require("config.catppuccin")
-
-vim.api.nvim_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
