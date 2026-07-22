a92cacdd6f7104289fb931513c4f1d5ba1008d94
Giuseppe Salvi <giuseppe.salvi98@gmail.com>
2026-07-22 16:22:49 +0200

    Update local Neovim config

--- lua/config/catppuccin.lua
+++ lua/config/catppuccin.lua
@@ -1,17 +1,70 @@
 require("catppuccin").setup({
   flavour = "macchiato", -- latte, frappe, macchiato, mocha
-  background = { -- :h background
+  background = {
     light = "latte",
-    dark = "mocha",
+    dark = "macchiato",
   },
-  transparent_background = false,
+  transparent_background = true, -- Enable transparency to work with terminal
+  show_end_of_buffer = false,
   term_colors = true,
+  dim_inactive = {
+    enabled = false,
+    shade = "dark",
+    percentage = 0.15,
+  },
+  styles = {
+    comments = { "italic" },
+    conditionals = { "italic" },
+    loops = {},
+    functions = {},
+    keywords = { "bold" },
+    strings = {},
+    variables = {},
+    numbers = {},
+    booleans = { "bold" },
+    properties = {},
+    types = { "bold" },
+    operators = {},
+  },
   integrations = {
     cmp = true,
     gitsigns = true,
     nvimtree = true,
-    telescope = true,
+    telescope = {
+      enabled = true,
+    },
     mason = true,
+    treesitter = true,
+    treesitter_context = true,
+    which_key = true,
+    indent_blankline = {
+      enabled = true,
+      scope_color = "lavender",
+      colored_indent_levels = false,
+    },
+    native_lsp = {
+      enabled = true,
+      virtual_text = {
+        errors = { "italic" },
+        hints = { "italic" },
+        warnings = { "italic" },
+        information = { "italic" },
+      },
+      underlines = {
+        errors = { "underline" },
+        hints = { "underline" },
+        warnings = { "underline" },
+        information = { "underline" },
+      },
+    },
+    mini = {
+      enabled = true,
+      indentscope_color = "lavender",
+    },
+    dashboard = true,
+    noice = true,
+    notify = true,
+    flash = true,
   },
 })
 
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,10 +1,27 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    vim.g.clipboard = {
+      name = "OSC 52",
+      copy = {
+        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+      },
+      paste = {
+        ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+      },
+    }
+  end,
+})
+
+vim.opt.clipboard = "unnamedplus"
 
 -- Spelling configuration
 vim.opt.spell = true
-vim.opt.spelllang = { 'it', 'en_us' }
+vim.opt.spelllang = { "it", "en_us" }
 
 -- Filter out MD051 markdownlint errors
 vim.diagnostic.config({
@@ -12,21 +29,21 @@ vim.diagnostic.config({
     filter = function(diagnostic)
       -- Filter out MD051 errors from markdownlint
       return not (diagnostic.source == "markdownlint" and diagnostic.code == "MD051")
-    end
+    end,
   },
   signs = {
     filter = function(diagnostic)
       return not (diagnostic.source == "markdownlint" and diagnostic.code == "MD051")
-    end
+    end,
   },
   underline = {
     filter = function(diagnostic)
       return not (diagnostic.source == "markdownlint" and diagnostic.code == "MD051")
-    end
+    end,
   },
   float = {
     filter = function(diagnostic)
       return not (diagnostic.source == "markdownlint" and diagnostic.code == "MD051")
-    end
-  }
+    end,
+  },
 })
